8a4f786db6600f5db12a7061937c04e6a9c90515
SigursteinnOli <sigursteinn.oli.t@gmail.com>
2019-09-27 11:30:59 +0000

    Combo System

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,57 +6,61 @@ public class PlayerController : MonoBehaviour
 {
     public float WalkingSpeed = 3f;
     public float RunningSpeed = 6f;
-    
+
     public Animator anim;
 
     private Rigidbody PlayerRB;
     private Vector3 movement;
-    public bool InCombat; 
+    public bool InCombat;
+    public int AttackNr=0;
+    public float ComboTimeLeft = 3f;
+    private int AttackHolder;
 
 
     private void Start()
     {
         PlayerRB = GetComponent<Rigidbody>();
         InCombat = false;
+
     }
 
     private void FixedUpdate()
     {
-        
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        if(InCombat==true)
+        if (InCombat == true)
         {
             Combat();
         }
 
-    
-        if(InCombat==false)
-            {
-               if ((Input.GetKey("w") == false) && (Input.GetKey("a") == false) && (Input.GetKey("s") == false) && (Input.GetKey("d") == false))
-                {
-                    PlayerRB.constraints = RigidbodyConstraints.FreezePositionX;
-                    PlayerRB.constraints = RigidbodyConstraints.FreezePositionZ;
-                }
 
-                else
-                {
-                  PlayerRB.constraints = RigidbodyConstraints.None;
-                  Move(moveHorizontal, moveVertical);
-                } 
-            }
-        
-        if(Input.GetKeyDown("space"))
+        if (InCombat == false)
         {
-            if(InCombat==true){InCombat=false;}
-            
-            else{InCombat=true;}
-            
+            if ((Input.GetKey("w") == false) && (Input.GetKey("a") == false) && (Input.GetKey("s") == false) && (Input.GetKey("d") == false))
+            {
+                PlayerRB.constraints = RigidbodyConstraints.FreezePositionX;
+                PlayerRB.constraints = RigidbodyConstraints.FreezePositionZ;
+            }
+
+            else
+            {
+                PlayerRB.constraints = RigidbodyConstraints.None;
+                Move(moveHorizontal, moveVertical);
+            }
         }
-        
-        
-        
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (InCombat == true) { InCombat = false; }
+
+            else { InCombat = true; }
+
+        }
+
+
+
     }
 
     private void Move(float moveHorizontal, float moveVertical)
@@ -77,9 +81,26 @@ public class PlayerController : MonoBehaviour
 
     private void Combat()
     {
-        anim.SetBool("Kick",false);
-        anim.SetBool("Combat-Idle",true);
-        if(Input.GetKeyDown("e")){anim.SetBool("Kick",true);}
+        AttackHolder = 0;
+        anim.SetInteger("AttackNr", 0);
+        CombatCounter();
+        anim.SetBool("Kick", false);
+        anim.SetBool("Combat-Idle", true);
+        if (Input.GetKeyDown("e"))
+        {
 
+            if(ComboTimeLeft > 0) { AttackNr++; }
+            anim.SetInteger("AttackNr", AttackNr);
+        }
+        if(ComboTimeLeft < 0) { AttackNr = 0;}
     }
+
+    private void CombatCounter()
+    {
+        
+        ComboTimeLeft -= Time.deltaTime;
+        if (Input.GetKeyDown("e")){ComboTimeLeft = 3f;}
+    }
+
+    
 }

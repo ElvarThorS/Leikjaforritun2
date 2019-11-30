2a93235d4a7eaa64b00099c7a728126024d3c133
Elvar <elvar-thor@hotmail.com>
2019-11-30 13:48:48 +0000

    Init

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform Target;
+    public float FollowSpeed;
+    public Vector3 Offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 GoalPosition = Target.position + Offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, GoalPosition, FollowSpeed);
+        transform.position = SmoothedPosition;
+
+        
+    }
+}

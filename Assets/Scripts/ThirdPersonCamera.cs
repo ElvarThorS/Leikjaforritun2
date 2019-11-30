23819a8d0390e167c4558f16a25da70883fa6760
Elvar <elvar-thor@hotmail.com>
2019-11-30 14:57:29 +0000

    init

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -4,41 +4,6 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    private const float Y_ANGLE_MIN = 5.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
-
-    public Transform lookAt;
-    public Transform camTransform;
-
-    private Camera cam;
-
-    private float distance = 10.0f;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
-    private float sensivityX = 4.0f;
-    private float sensivityY = 1.0f;
-
-    void Start()
-    {
-        camTransform = transform;
-        cam = Camera.main;
-    }
-
-    private void Update()
-    {
-        currentX += Input.GetAxis("Mouse X");
-        currentY += Input.GetAxis("Mouse Y")*-1;
-
-        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
-    }
-
-    private void LateUpdate()
-    {
-        Vector3 dir = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        camTransform.position = lookAt.position + rotation * dir;
-        camTransform.LookAt(lookAt.position);
-    }
     public Transform target;
 
     public Vector3 CameraOffset;
@@ -53,9 +18,9 @@ public class ThirdPersonCamera : MonoBehaviour
     public float minViewAngle;
 
     public bool invertY;
-    void Start ()
+    void Start()
     {
-        if(!useOffsetValues)
+        if (!useOffsetValues)
         {
             CameraOffset = target.position - transform.position;
         }
@@ -63,7 +28,7 @@ public class ThirdPersonCamera : MonoBehaviour
         pivot.transform.position = target.transform.position;
         //pivot.transform.parent = target.transform;
         pivot.transform.parent = null;
-        
+
         //Makes the cursor disappear when in play mode
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -83,20 +48,21 @@ public class ThirdPersonCamera : MonoBehaviour
         if (invertY)
         {
             pivot.Rotate(vertical, 0, 0);
-        } else
+        }
+        else
         {
             pivot.Rotate(-vertical, 0, 0);
         }
 
         //Limit the up/down camera rotation
-        if (pivot.rotation.eulerAngles.x > maxViewAngle  && pivot.rotation.eulerAngles.x < 180f)
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
         {
             pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
         }
 
-        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f+minViewAngle)
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
         {
-            pivot.rotation = Quaternion.Euler(360f+minViewAngle, 0, 0);
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
         }
 
         float desiredYAngle = pivot.eulerAngles.y;
@@ -104,14 +70,13 @@ public class ThirdPersonCamera : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotation * CameraOffset);
 
-        if(transform.position.y < target.position.y)
+        if (transform.position.y < target.position.y)
         {
-            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
         }
 
         transform.LookAt(target);
     }
 
-    
->>>>>>> bb7521c4f21294206e67d3209d119b2d3d1fc728
+
 }
